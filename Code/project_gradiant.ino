feb3049d579c824e6044d2f0b06939e2fad4f779
aleparuokakauppa <aleksileinonen2@gmail.com>
2024-04-09 15:23:58 +0300

    Physical testing

--- Code/project_gradiant.ino
+++ Code/project_gradiant.ino
@@ -1,23 +1,13 @@
 #include <Adafruit_NeoPixel.h>
 #define NEOPIXEL_PIN 8
 #define PIXEL_COUNT 4
-#define TEMP_SENSOR_PIN 28
+#define TEMP_SENSOR_PIN 28 // Should be a pin that supports ADC!
+#define MAX_ADC 4095
+#define MIN_ADC 0
+#define MAX_SENSOR_TEMP 125
+#define MIN_SENSOR_TEMP -40
 
 Adafruit_NeoPixel strip(PIXEL_COUNT, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
-
-const float BETA = 3950;
-
-float readCelsius() {
-    int analogValue = analogRead(TEMP_SENSOR_PIN);
-    return 1 / (log(1 / (1023. / analogValue - 1)) / BETA + 1.0 / 298.15) - 273.15;
-}
-
-void setup() {
-    pinMode(NEOPIXEL_PIN, OUTPUT);
-    pinMode(TEMP_SENSOR_PIN, INPUT);
-    strip.begin();
-    strip.show();
-}
 
 int scaledValue(int val, int v1min, int v1max, int v2min, int v2max) {
     int v1range = v1max - v1min;
@@ -26,19 +16,38 @@ int scaledValue(int val, int v1min, int v1max, int v2min, int v2max) {
     return (val - v1min) * ratio + v2min;
 }
 
+const double cToDigRatio = 24.81
+void setup() {
+    // Can be defined as 397.18182 at start
+    // ratio = (MAX_ADC - MIN_ADC) / (MAX_SENSOR_TEMP - MIN_SENSOR_TEMP)
+    // (65335 - 0) / (125 - (-40)) = 397.181818181818182
+    Serial.begin(9600);
+    pinMode(NEOPIXEL_PIN, OUTPUT);
+    pinMode(TEMP_SENSOR_PIN, INPUT);
+    strip.begin();
+    strip.show();
+}
+
+int readTemperature() {
+    return static_cast<int>(analogRead(TEMP_SENSOR_PIN) * cToDigRatio);
+}
+
 void loop() {
-    int currTemp = (int)readCelsius();
+    int currTemp = readTemperature();
+    const int minTemp = -20;
+    const int maxTemp = 20;
     int rgbScaled;
-    if (currTemp < -20) {
+    if (currTemp < minTemp) {
         rgbScaled = 0;
-    } else if (currTemp > 20) {
+    } else if (currTemp > maxTemp) {
         rgbScaled = 255;
     } else {
-        rgbScaled = scaledValue(currTemp, -20, 20, 0, 255);
+        rgbScaled = scaledValue(currTemp, minTemp, maxTemp, 0, 255);
     }
     for (int currPx = 0; currPx < PIXEL_COUNT; currPx++) {
         strip.setPixelColor(currPx, rgbScaled, 0, 255-rgbScaled);
     }
     strip.show();
-    delay(20);
+    // Doesn't need to run as fast as possible
+    delay(1000);
 }
